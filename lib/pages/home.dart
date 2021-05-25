80673001342e2cf342c8459b9f442a68146a0092
Marcio <marciojose19@hotmail.com>
2021-05-25 18:56:56 -0500

    feat: BottomNavigationBar Widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,13 +8,77 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Explore',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Favourites',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Search',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Achievements',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_airport),
+            label: 'Explore',
+            backgroundColor: Color(0xff2365FF),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favourites',
+            backgroundColor: Color(0xff2365FF),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+            backgroundColor: Color(0xff2365FF),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check_box),
+            label: 'Achievements',
+            backgroundColor: Color(0xff2365FF),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Color(0xff2365FF),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
